e41dc41b41646fbc3445bd18f6c350b9b9afe8ee
kobe <thodavid@student.42.fr>
2026-03-05 15:39:42 +0100

    Refactor main.cpp for input handling and pairing logic

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -12,19 +12,6 @@
 
 #include "PmergeMe.hpp"
 
-/*
-1. Pairing : make pair & compare each pair → (loser, winner) loser < winner
-
-2. Rec_Desc : trier recursivement(step 1.) la liste des winners (ce qui reduit le problèm)
-                => Base case : 1winner
-
-3. Unwinding / insertion :
-    3.0 insertion loser associé au plus petit winner au début (placement certain 0 comparaisons)
-    3.1 insertion autres losers (+ l’orphelin) dans un ordre -> déterminé par un schéma basé sur JacobsthalList
-    3.2 chaque loser (y_i) est inséré par binarySearch dans S[0 .. x_i) (borne exclue), 
-        et l’orphelin dans toute `S`.
-*/
-
 //input X = { 42 , 3 , 1 , 2 , 4 }
 
 int main(int ac, char **av){
@@ -34,26 +21,28 @@ int main(int ac, char **av){
         if(ac <= 2)
             error("Please provide an list of integer (mini 2 int)");
         
-        std::vector<int> s_main;
-        s_main.reserve(ac - 1);
-
-        input_to_vector(ac, av, s_main);
-
-        /* 1.PAIRING */
-        bool    has_orphan = false;
-        int     orphan = 0;
-        std::vector<std::pair< int, int> > s_pair;
-        s_pair.reserve((s_main.size() / 2));
+        std::vector<int> input_v;
+        input_v.reserve(ac - 1);
+        input_to_vector(ac, av, input_v);
         
-        //pairing(s_main, s_pair, has_orphan, orphan);
+        /* odd input */
+        bool has_orphan = 0;
+        int  orphan = 0;
+
+        /* PAIRING */
+        std::vector<PairNode> pairs_v;
+        pairs_v.reserve(input_v.size() / 2);
+        pairing(input_v, pairs_v, has_orphan, orphan);
         
-        /* 2~3. REC */
-        lets_cook(s_main, s_pair, has_orphan, orphan);
+        
+        std::vector<PairNode> winners;
+        build_winners_v(pairs_v, winners);
+
+        std::vector<PairNode> w = fj_sort_winners(winners, pairs_v.size());
 
         /* DEBUG */
-        //print_arg(av);
-        print_v(s_main);
-        print_vp(s_pair);
+        for(size_t i = 0; i < w.size(); ++i)
+            std::cout << "Winner h:: " << w[i].winner << "  " << w[i].looser << " | id:: " << w[i].id << '\n';
     }
 	catch (std::runtime_error& e){
 		std::cerr << "Error: " << e.what() << '\n';
@@ -61,12 +50,3 @@ int main(int ac, char **av){
 	}
     return (0);
 }
-
-
-/*
-    -> comment inserer les loosers associe au plus petit winner lors de l'unwinding?
-    -> comment gerer les associations??
-    -> comment ne pas perdre les loosers
-    -> Jacobstal en dur????
-
-*/
